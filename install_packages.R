e85825c3b955e8f7f71d2798e55df60a432dc7c6
Logan Lossing <loganlossing@gmail.com>
2021-01-20 17:48:08 -0800

    Initial commit

---
+++ install_packages.R
@@ -0,0 +1,9 @@
+remotes::install_github("rstudio/distill")
+remotes::install_github("rstudio/rmarkdown")
+install.packages("postcards")
+install.packages("testthat")
+
+
+
+library(postcards)
+library(distill)
